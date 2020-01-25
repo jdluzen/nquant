d2daaea3ae58d1e77ac68e850e12ecf388d9624d
Joe Dluzen <jdluzen@gmail.com>
2020-01-25 10:59:33 -0500

    chore(setup): initial commit
    ...from 2013

---
+++ NQuantAot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using nQuant;
+
+namespace NQuant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+            foreach (string arg in args)
+            {
+                WuQuantizer q = new WuQuantizer();
+                Bitmap b = (Bitmap)Bitmap.FromFile(arg);
+                if (b.PixelFormat != PixelFormat.Format32bppArgb)
+                {
+                    Bitmap b2 = new Bitmap(b.Width, b.Height, PixelFormat.Format32bppArgb);
+                    using (Graphics g = Graphics.FromImage(b2))
+                        g.DrawImage(b, new Rectangle(0, 0, b2.Width, b2.Height));
+                    b.Dispose();
+                    b = b2;
+                }
+                Image i = q.QuantizeImage(b, 0, 1);//FIXME: make options
+                i.Save(Path.GetFileNameWithoutExtension(arg) + Environment.TickCount + ".png", ImageFormat.Png);
+            }
+        }
+    }
+}
